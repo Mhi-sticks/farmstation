37023ecb562ef54da260ddc388b57cb248679f27
Daniel <adeosundaniel33@gmail.com>
2023-04-27 09:41:45 +0100

    set the mongoose strict query to be false

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
-const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URL,
-    { useNewUrlParser: true, });
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true });
 
 const db = mongoose.connection;
 db.once("open", function () {
