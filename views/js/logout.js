8016801afd104ca0d96beab1fd4a5f38a9c2dc74
ExtranoDev <jeomade19@gmail.com>
2023-04-28 03:07:19 -0700

    Edit logout

--- views/js/logout.js
+++ views/js/logout.js
@@ -1,5 +1,5 @@
 const form = document.querySelector("form"); // select the form element
-const loginButton = form.querySelector(".logout-btn"); // select the submit button
+const logoutButton = form.querySelector(".logout-btn"); // select the submit button
 
 logoutButton.addEventListener("click", (e) => {
   e.preventDefault(); // prevent the default form submission behavior
